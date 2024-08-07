88bd00ca2802b114767a788ad9254cad597867db
huynhminh24 <manager.hvm@gmail.com>
2024-08-07 12:06:36 +0700

    BaiTap

---
+++ Buoi3/Bai3.cpp
@@ -0,0 +1,382 @@
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <stdbool.h>
+//
+//#define MAX 100
+//
+//// Hàm tạo ma trận ngẫu nhiên từ a đến b
+//void taoMaTran(int a[MAX][MAX], int m, int n, int a_val, int b_val) {
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            a[i][j] = a_val + rand() % (b_val - a_val + 1);
+//        }
+//    }
+//}
+//
+//// Hàm xuất ma trận
+//void xuatMaTran(int a[MAX][MAX], int m, int n) {
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            printf("%d\t", a[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+//
+//// Xuất các cột chỉ chứa số lẻ
+//void xuatCotChiChuaSoLe(int a[MAX][MAX], int m, int n) {
+//    bool hasElement = false;
+//    printf("1. Cac cot chi chua so le:\n");
+//    for (int j = 0; j < n; j++) {
+//        bool chiSoLe = true;
+//        for (int i = 0; i < m; i++) {
+//            if (a[i][j] % 2 == 0) {
+//                chiSoLe = false;
+//                break;
+//            }
+//        }
+//        if (chiSoLe) {
+//            hasElement = true;
+//            printf("Cot %d: ", j);
+//            for (int i = 0; i < m; i++) {
+//                printf("%d ", a[i][j]);
+//            }
+//            printf("\n");
+//        }
+//    }
+//    if (!hasElement) {
+//        printf("Khong tim thay!\n");
+//    }
+//}
+//
+//// Tìm phần tử lớn nhất trong các phần tử trên biên của ma trận
+//int timMaxTrenBien(int a[MAX][MAX], int m, int n) {
+//    int max = a[0][0];
+//    // Duyệt các phần tử trên biên
+//    for (int j = 0; j < n; j++) {
+//        if (a[0][j] > max) max = a[0][j];
+//        if (a[m - 1][j] > max) max = a[m - 1][j];
+//    }
+//    for (int i = 1; i < m - 1; i++) {
+//        if (a[i][0] > max) max = a[i][0];
+//        if (a[i][n - 1] > max) max = a[i][n - 1];
+//    }
+//    return max;
+//}
+//
+//// Trong ma trận có bao nhiêu phần tử có chữ số 2 xuất hiện trong các chữ số của nó
+//int demPhanTuCoChuSo2(int a[MAX][MAX], int m, int n) {
+//    int dem = 0;
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            int num = a[i][j];
+//            while (num != 0) {
+//                if (num % 10 == 2) {
+//                    dem++;
+//                    break;
+//                }
+//                num /= 10;
+//            }
+//        }
+//    }
+//    return dem;
+//}
+//
+//// Xuất các phần tử cực tiểu của ma trận
+//void xuatCucTieu(int a[MAX][MAX], int m, int n) {
+//    printf("4. Cac phan tu cuc tieu:\n");
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            bool cucTieu = true;
+//            int di[] = { -1, 0, 1, 0 };
+//            int dj[] = { 0, 1, 0, -1 };
+//            for (int k = 0; k < 4; k++) {
+//                int ni = i + di[k];
+//                int nj = j + dj[k];
+//                if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+//                    if (a[i][j] >= a[ni][nj]) {
+//                        cucTieu = false;
+//                        break;
+//                    }
+//                }
+//            }
+//            if (cucTieu) {
+//                printf("a[%d][%d] = %d\n", i, j, a[i][j]);
+//            }
+//        }
+//    }
+//}
+//
+//// Sắp xếp ma trận sao cho: các dòng có chỉ số lẻ thì tăng dần, còn các dòng có chỉ số chẵn thì giảm dần
+//void sapXepMaTranDongLeTangChanGiam(int a[MAX][MAX], int m, int n) {
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n - 1; j++) {
+//            for (int k = j + 1; k < n; k++) {
+//                if ((i % 2 == 1 && a[i][j] > a[i][k]) || (i % 2 == 0 && a[i][j] < a[i][k])) {
+//                    int temp = a[i][j];
+//                    a[i][j] = a[i][k];
+//                    a[i][k] = temp;
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//// Sắp xếp ma trận sao cho: các cột có chỉ số lẻ thì giảm dần, còn các cột có chỉ số chẵn thì tăng dần
+//void sapXepMaTranCotLeGiamChanTang(int a[MAX][MAX], int m, int n) {
+//    for (int j = 0; j < n; j++) {
+//        for (int i = 0; i < m - 1; i++) {
+//            for (int k = i + 1; k < m; k++) {
+//                if ((j % 2 == 1 && a[i][j] < a[k][j]) || (j % 2 == 0 && a[i][j] > a[k][j])) {
+//                    int temp = a[i][j];
+//                    a[i][j] = a[k][j];
+//                    a[k][j] = temp;
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//// Kiểm tra các giá trị trong ma trận có giảm dần theo cột và dòng (ziczac)
+//bool kiemTraZicZac(int a[MAX][MAX], int m, int n) {
+//    for (int i = 0; i < m - 1; i++) {
+//        for (int j = 0; j < n - 1; j++) {
+//            if (!(a[i][j] > a[i][j + 1] && a[i][j] > a[i + 1][j])) {
+//                return false;
+//            }
+//        }
+//    }
+//    return true;
+//}
+//
+//// Liệt kê chỉ số các dòng chứa toàn giá trị chẵn
+//void lietKeDongToanChan(int a[MAX][MAX], int m, int n) {
+//    bool hasElement = false;
+//    printf("8. Cac dong chi chua so chan:\n");
+//    for (int i = 0; i < m; i++) {
+//        bool toanChan = true;
+//        for (int j = 0; j < n; j++) {
+//            if (a[i][j] % 2 != 0) {
+//                toanChan = false;
+//                break;
+//            }
+//        }
+//        if (toanChan) {
+//            hasElement = true;
+//            printf("Dong %d\n", i);
+//        }
+//    }
+//    if (!hasElement) {
+//        printf("Khong tim thay!\n");
+//    }
+//}
+//
+//// Liệt kê các dòng chứa giá trị giảm dần
+//void lietKeDongGiamDan(int a[MAX][MAX], int m, int n) {
+//    bool hasElement = false;
+//    printf("9. Cac dong co gia tri giam dan:\n");
+//    for (int i = 0; i < m; i++) {
+//        bool giamDan = true;
+//        for (int j = 0; j < n - 1; j++) {
+//            if (a[i][j] < a[i][j + 1]) {
+//                giamDan = false;
+//                break;
+//            }
+//        }
+//        if (giamDan) {
+//            hasElement = true;
+//            printf("Dong %d\n", i);
+//        }
+//    }
+//    if (!hasElement) {
+//        printf("Khong tim thay!\n");
+//    }
+//}
+//
+//// Tìm giá trị xuất hiện nhiều nhất trong ma trận
+//int giaTriXuatHienNhieuNhat(int a[MAX][MAX], int m, int n) {
+//    int freq[MAX * MAX] = { 0 };
+//    int maxFreq = 0, value = a[0][0];
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            freq[a[i][j]]++;
+//            if (freq[a[i][j]] > maxFreq) {
+//                maxFreq = freq[a[i][j]];
+//                value = a[i][j];
+//            }
+//        }
+//    }
+//    return value;
+//}
+//
+//// Tìm các chữ số xuất hiện nhiều nhất trong ma trận
+//void chuSoXuatHienNhieuNhat(int a[MAX][MAX], int m, int n) {
+//    int freq[10] = { 0 };
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            int num = a[i][j];
+//            while (num != 0) {
+//                freq[num % 10]++;
+//                num /= 10;
+//            }
+//        }
+//    }
+//    int maxFreq = 0;
+//    for (int i = 0; i < 10; i++) {
+//        if (freq[i] > maxFreq) {
+//            maxFreq = freq[i];
+//        }
+//    }
+//    printf("11. Cac chu so xuat hien nhieu nhat: ");
+//    for (int i = 0; i < 10; i++) {
+//        if (freq[i] == maxFreq) {
+//            printf("%d ", i);
+//        }
+//    }
+//    printf("\n");
+//}
+//
+//// Liệt kê các cột có tổng nhỏ nhất trong ma trận
+//void lietKeCotTongNhoNhat(int a[MAX][MAX], int m, int n) {
+//    int minSum = 0;
+//    for (int i = 0; i < m; i++) {
+//        minSum += a[i][0];
+//    }
+//    for (int j = 1; j < n; j++) {
+//        int sum = 0;
+//        for (int i = 0; i < m; i++) {
+//            sum += a[i][j];
+//        }
+//        if (sum < minSum) {
+//            minSum = sum;
+//        }
+//    }
+//    printf("12. Cac cot co tong nho nhat: ");
+//    for (int j = 0; j < n; j++) {
+//        int sum = 0;
+//        for (int i = 0; i < m; i++) {
+//            sum += a[i][j];
+//        }
+//        if (sum == minSum) {
+//            printf("%d ", j);
+//        }
+//    }
+//    printf("\n");
+//}
+//
+//// Hoán vị hai cột i và j trong ma trận
+//void hoanViCot(int a[MAX][MAX], int m, int n, int i, int j) {
+//    if (i >= n || j >= n || i < 0 || j < 0) {
+//        printf("Chi so cot khong hop le\n");
+//        return;
+//    }
+//    for (int k = 0; k < m; k++) {
+//        int temp = a[k][i];
+//        a[k][i] = a[k][j];
+//        a[k][j] = temp;
+//    }
+//}
+//
+//// Hoán vị hai dòng k và l trong ma trận
+//void hoanViDong(int a[MAX][MAX], int m, int n, int k, int l) {
+//    if (k >= m || l >= m || k < 0 || l < 0) {
+//        printf("Chi so dong khong hop le\n");
+//        return;
+//    }
+//    for (int i = 0; i < n; i++) {
+//        int temp = a[k][i];
+//        a[k][i] = a[l][i];
+//        a[l][i] = temp;
+//    }
+//}
+//
+//int main() {
+//    srand(time(NULL));
+//
+//    int a[MAX][MAX];
+//    int m, n, a_val, b_val;
+//
+//    // Nhập kích thước ma trận
+//    printf("Nhap so dong (m) va so cot (n): ");
+//    if (scanf("%d %d", &m, &n) != 2 || m <= 0 || n <= 0 || m > MAX || n > MAX) {
+//        printf("Nhap khong hop le!\n");
+//        return 1;
+//    }
+//
+//    // Nhập giá trị ngẫu nhiên
+//    printf("Nhap gia tri bat dau (a) va gia tri ket thuc (b): ");
+//    if (scanf("%d %d", &a_val, &b_val) != 2 || a_val > b_val) {
+//        printf("Nhap khong hop le!\n");
+//        return 1;
+//    }
+//
+//    // Tạo ma trận ngẫu nhiên
+//    taoMaTran(a, m, n, a_val, b_val);
+//
+//    // Xuất ma trận
+//    printf("Ma tran ngau nhien:\n");
+//    xuatMaTran(a, m, n);
+//
+//    // Xuất các cột chỉ chứa số lẻ
+//    xuatCotChiChuaSoLe(a, m, n);
+//
+//    // Tìm phần tử lớn nhất trong các phần tử trên biên của ma trận
+//    int maxBien = timMaxTrenBien(a, m, n);
+//    printf("2. Phan tu lon nhat tren bien: %d\n", maxBien);
+//
+//    // Đếm các phần tử có chữ số 2 xuất hiện trong các chữ số của nó
+//    int demChuSo2 = demPhanTuCoChuSo2(a, m, n);
+//    printf("3. So phan tu co chu so 2: %d\n", demChuSo2);
+//
+//    // Xuất các phần tử cực tiểu của ma trận
+//    xuatCucTieu(a, m, n);
+//
+//    // Sắp xếp ma trận sao cho: các dòng có chỉ số lẻ thì tăng dần, còn các dòng có chỉ số chẵn thì giảm dần
+//    sapXepMaTranDongLeTangChanGiam(a, m, n);
+//    printf("5. Ma tran sau khi sap xep dong le tang, chan giam:\n");
+//    xuatMaTran(a, m, n);
+//
+//    // Sắp xếp ma trận sao cho: các cột có chỉ số lẻ thì giảm dần, còn các cột có chỉ số chẵn thì tăng dần
+//    sapXepMaTranCotLeGiamChanTang(a, m, n);
+//    printf("6. Ma tran sau khi sap xep cot le giam, chan tang:\n");
+//    xuatMaTran(a, m, n);
+//
+//    // Kiểm tra các giá trị trong ma trận có giảm dần theo cột và dòng (ziczac)
+//    bool ziczac = kiemTraZicZac(a, m, n);
+//    printf("7. Ma tran giam dan theo cot va dong (ziczac): %s\n", ziczac ? "Co!" : "Khong co!");
+//
+//    // Liệt kê chỉ số các dòng chứa toàn giá trị chẵn
+//    lietKeDongToanChan(a, m, n);
+//
+//    // Liệt kê các dòng chứa giá trị giảm dần
+//    lietKeDongGiamDan(a, m, n);
+//
+//    // Tìm giá trị xuất hiện nhiều nhất trong ma trận
+//    int giaTriNhieuNhat = giaTriXuatHienNhieuNhat(a, m, n);
+//    printf("10. Gia tri xuat hien nhieu nhat: %d\n", giaTriNhieuNhat);
+//
+//    // Tìm các chữ số xuất hiện nhiều nhất trong ma trận
+//    chuSoXuatHienNhieuNhat(a, m, n);
+//
+//    // Liệt kê các cột có tổng nhỏ nhất trong ma trận
+//    lietKeCotTongNhoNhat(a, m, n);
+//
+//    // Hoán vị hai cột i và j trong ma trận
+//    int i, j;
+//    printf("13. Nhap chi so hai cot can hoan vi: ");
+//    scanf("%d %d", &i, &j);
+//    hoanViCot(a, m, n, i, j);
+//    printf("Ma tran sau khi hoan vi cot %d va %d:\n", i, j);
+//    xuatMaTran(a, m, n);
+//
+//    // Hoán vị hai dòng k và l trong ma trận
+//    int k, l;
+//    printf("14. Nhap chi so hai dong can hoan vi: ");
+//    scanf("%d %d", &k, &l);
+//    hoanViDong(a, m, n, k, l);
+//    printf("Ma tran sau khi hoan vi dong %d va %d:\n", k, l);
+//    xuatMaTran(a, m, n);
+//
+//    return 0;
+//}
---
+++ Buoi3/Bai4.cpp
@@ -0,0 +1,290 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define MIN_N 5
+
+// Hàm tạo ma trận vuông với số nguyên ngẫu nhiên
+void taoMaTran(int n, int** a) {
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = rand() % 100; // số ngẫu nhiên trong khoảng 0-99
+        }
+    }
+}
+
+// Hàm xuất ma trận
+void xuatMaTran(int n, int** a) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Hàm sắp xếp đường chéo phụ tăng dần
+void sapXepDuongCheoPhuTang(int n, int** a) {
+    int* duongCheoPhu = (int*)malloc(2 * n * sizeof(int)); // Tối đa 2n phần tử
+
+    int index = 0;
+    // Lấy các phần tử trên đường chéo phụ
+    for (int s = 0; s < n; s++) {
+        int i = s;
+        int j = 0;
+        while (i >= 0) {
+            duongCheoPhu[index++] = a[i][j++];
+            i--;
+        }
+    }
+
+    for (int s = 1; s < n; s++) {
+        int i = n - 1;
+        int j = s;
+        while (j < n) {
+            duongCheoPhu[index++] = a[i--][j++];
+        }
+    }
+
+    // Sắp xếp tăng dần
+    for (int i = 0; i < index - 1; i++) {
+        for (int j = i + 1; j < index; j++) {
+            if (duongCheoPhu[i] > duongCheoPhu[j]) {
+                int temp = duongCheoPhu[i];
+                duongCheoPhu[i] = duongCheoPhu[j];
+                duongCheoPhu[j] = temp;
+            }
+        }
+    }
+
+    index = 0;
+    for (int s = 0; s < n; s++) {
+        int i = s;
+        int j = 0;
+        while (i >= 0) {
+            a[i][j++] = duongCheoPhu[index++];
+            i--;
+        }
+    }
+
+    for (int s = 1; s < n; s++) {
+        int i = n - 1;
+        int j = s;
+        while (j < n) {
+            a[i--][j++] = duongCheoPhu[index++];
+        }
+    }
+
+    free(duongCheoPhu);
+}
+
+// Hàm sắp xếp ma trận sao cho các dòng có chỉ số lẻ thì tăng dần, còn các dòng có chỉ số chẵn thì giảm dần
+void sapXepDongTheoChiSo(int n, int** a) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if ((i % 2 == 0 && a[i][j] < a[i][k]) || (i % 2 != 0 && a[i][j] > a[i][k])) {
+                    int temp = a[i][j];
+                    a[i][j] = a[i][k];
+                    a[i][k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Hàm sắp xếp ma trận sao cho các cột có chỉ số lẻ thì giảm dần, còn các cột có chỉ số chẵn thì tăng dần
+void sapXepCotTheoChiSo(int n, int** a) {
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int k = i + 1; k < n; k++) {
+                if ((j % 2 == 0 && a[i][j] > a[k][j]) || (j % 2 != 0 && a[i][j] < a[k][j])) {
+                    int temp = a[i][j];
+                    a[i][j] = a[k][j];
+                    a[k][j] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Hàm sắp xếp ma trận sao cho các phần tử trên các đường chéo chính và các đường chéo song song với đường chéo chính tăng dần
+void sapXepDuongCheoChinhTang(int n, int** a) {
+    int* duongCheoChinh = (int*)malloc(2 * n * sizeof(int)); // Tối đa 2n phần tử
+
+    int index = 0;
+    // Lấy các phần tử trên đường chéo chính và các đường chéo song song
+    for (int s = 0; s < n; s++) {
+        int i = s;
+        int j = 0;
+        while (i >= 0) {
+            duongCheoChinh[index++] = a[i][j++];
+            i--;
+        }
+    }
+
+    for (int s = 1; s < n; s++) {
+        int i = n - 1;
+        int j = s;
+        while (j < n) {
+            duongCheoChinh[index++] = a[i--][j++];
+        }
+    }
+
+    // Sắp xếp tăng dần
+    for (int i = 0; i < index - 1; i++) {
+        for (int j = i + 1; j < index; j++) {
+            if (duongCheoChinh[i] > duongCheoChinh[j]) {
+                int temp = duongCheoChinh[i];
+                duongCheoChinh[i] = duongCheoChinh[j];
+                duongCheoChinh[j] = temp;
+            }
+        }
+    }
+
+    index = 0;
+    for (int s = 0; s < n; s++) {
+        int i = s;
+        int j = 0;
+        while (i >= 0) {
+            a[i][j++] = duongCheoChinh[index++];
+            i--;
+        }
+    }
+
+    for (int s = 1; s < n; s++) {
+        int i = n - 1;
+        int j = s;
+        while (j < n) {
+            a[i--][j++] = duongCheoChinh[index++];
+        }
+    }
+
+    free(duongCheoChinh);
+}
+
+// Hàm di chuyển các phần tử sao cho các phần tử chẵn nằm ở các dòng đầu mảng, các phần tử lẻ nằm ở các dòng cuối mảng
+void diChuyenChanLen(int n, int** a) {
+    int* temp = (int*)malloc(n * n * sizeof(int));
+    int index = 0;
+
+    // Lưu các số chẵn vào mảng tạm
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] % 2 == 0) {
+                temp[index++] = a[i][j];
+            }
+        }
+    }
+
+    // Lưu các số lẻ vào mảng tạm
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] % 2 != 0) {
+                temp[index++] = a[i][j];
+            }
+        }
+    }
+
+    index = 0;
+    // Đặt các số chẵn vào các dòng đầu của ma trận
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (index < n * n && a[i][j] % 2 == 0) {
+                a[i][j] = temp[index++];
+            }
+        }
+    }
+
+    // Đặt các số lẻ vào các dòng cuối của ma trận
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (index < n * n && a[i][j] % 2 != 0) {
+                a[i][j] = temp[index++];
+            }
+        }
+    }
+
+    free(temp);
+}
+
+// Hàm kiểm tra ma trận có đối xứng qua đường chéo chính không
+bool kiemTraDoiXung(int n, int** a) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int nhapGiaTri() {
+    int n;
+    int result;
+    char c;
+
+    do {
+        printf("Nhap cap cua ma tran (n >= %d): ", MIN_N);
+        result = scanf_s("%d", &n);
+
+        // Xử lý trường hợp nhập vào không phải số nguyên
+        while ((c = getchar()) != '\n' && c != EOF) {}
+
+        if (result != 1 || n < MIN_N) {
+            printf("Gia tri khong hop le. Vui long nhap lai.\n");
+        }
+    } while (result != 1 || n < MIN_N);
+
+    return n;
+}
+
+int main() {
+    int n = nhapGiaTri();
+
+    int** a = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        a[i] = (int*)malloc(n * sizeof(int));
+    }
+
+    taoMaTran(n, a);
+    printf("Ma tran ban dau:\n");
+    xuatMaTran(n, a);
+
+    sapXepDuongCheoPhuTang(n, a);
+    printf("1. Ma tran sau khi sap xep duong cheo phu tang dan:\n");
+    xuatMaTran(n, a);
+
+    sapXepDongTheoChiSo(n, a);
+    printf("2. Ma tran sau khi sap xep dong:\n");
+    xuatMaTran(n, a);
+
+    sapXepCotTheoChiSo(n, a);
+    printf("3. Ma tran sau khi sap xep cot:\n");
+    xuatMaTran(n, a);
+
+    sapXepDuongCheoChinhTang(n, a);
+    printf("4. Ma tran sau khi sap xep duong cheo chinh tang dan:\n");
+    xuatMaTran(n, a);
+
+    diChuyenChanLen(n, a);
+    printf("5. Ma tran sau khi di chuyen chan va le:\n");
+    xuatMaTran(n, a);
+
+    if (kiemTraDoiXung(n, a)) {
+        printf("6. Ma tran doi xung qua duong cheo chinh!\n");
+    }
+    else {
+        printf("6. Ma tran khong doi xung qua duong cheo chinh!\n");
+    }
+
+    for (int i = 0; i < n; i++) {
+        free(a[i]);
+    }
+    free(a);
+
+    return 0;
+}
